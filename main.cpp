30f2d475d12975c340f55ec12c7453ff20d9eb8b
Mike <thrillmeal@ua.ru>
2019-12-11 02:41:07 +0300

    improvements

--- main.cpp
+++ main.cpp
@@ -13,12 +13,20 @@ int main()
 {
     Tools* tools = Tools::getInstance();
     Logger* logger = Logger::getInstance();
-    string name;
-    string operation;
-    cin>>name>>operation;
-    Controller controllerBase(operation);
-
-
+    string *name = new string();
+    string *operation = new string();
+    bool choice = true;
+    Controller *controller;
+    while(choice){
+        cin>>name->data()>>operation->data();
+        if(string(name->data()) == "ex"){
+            choice = false;
+        }
+        controller = new Controller(operation->data());
+    }
+    delete name;
+    delete operation;
+    delete controller;
 //    tools.ReadAllFiles();
     return 0;
 }
